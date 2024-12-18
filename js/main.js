3dd9ed9a092e65d3b37a6d57b24c7bcd6441e565
Roma <romabelka43@gmail.com>
2024-12-19 00:29:39 +0300

    final implementation

--- js/main.js
+++ js/main.js
@@ -13,32 +13,69 @@ var btns = document.getElementsByClassName("feed-item");
 var span = document.getElementsByClassName("close")[0];
 
 for (let i = 0; i < btns.length; i++) {
-    btns[i].onclick = function(event) {
+    btns[i].onclick = function (event) {
         event.preventDefault();
         modal.style.display = "block";
     }
 }
 
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
 
+var swipersInner = [];
+
 var swiper2 = new Swiper(".swiper-modal", {
-    centeredSlides: true,
     slidesPerView: "auto",
     spaceBetween: 100,
-    allowTouchMove: false,
+    centeredSlides: true,
+    allowTouchMove: true,
     slideToClickedSlide: true,
-    allowSlidePrew: true,
-    allowSlideNext: true,
     thumbs: {
         swiper: swiper,
     },
+    on: {
+        reachBeginning: function () {
+            swipersInner[0].autoplay.start();
+        },
+        slideChange: function () {
+            swipersInner.forEach(function (innerSwiper) {
+                innerSwiper.autoplay.stop();
+            });
+
+            var activeInnerSwiper = this.slides[this.activeIndex].querySelector('.swiper-inner');
+            if (activeInnerSwiper) {
+                var activeSwiper = activeInnerSwiper.swiper;
+                activeSwiper.autoplay.start();
+            }
+        },
+    },
+});
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+var innerSwipers = document.querySelectorAll('.swiper-inner');
+innerSwipers.forEach(function (inner) {
+    var innerSwiper = new Swiper(inner, {
+        pagination: {
+            el: inner.querySelector('.swiper-pagination'),
+            clickable: true,
+        },
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                const activeBullet = inner.querySelector('.swiper-pagination-bullet-active::after');
+                const activeBulletElement = inner.querySelector('.swiper-pagination-bullet-active');
+                if (activeBulletElement) {
+                    activeBulletElement.style.setProperty("--progress", 1 - progress);
+                }
+            }
+        }
+    });
+    swipersInner.push(innerSwiper);
 });
